03722ac2a87f94dbb4f531852c9a93ed4cb3bc4d
Troy <tward4@tulane.edu>
2016-09-26 11:26:16 -0400

    Refactor into seperate methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,35 +2,27 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<Post> posts = new ArrayList<>();
 
-        loadPosts(posts);
+        File f = new File("posts.txt");
+        ArrayList<Post> posts = loadPosts(f);
 
         Scanner consoleScanner = new Scanner(System.in);
-
         int replyId = -1;
         while (true) {
-            System.out.println("Posts replying to " + replyId);
-            for (int i =0; i < posts.size(); i++) {
-                Post post = posts.get(i);
-                if(post.replyId == replyId) {
-                    System.out.printf("[%s] %s by %s\n", i, post.text, post.author);
-                }
-            }
-
-            System.out.println("The the id you want to see replies to:");
-            replyId = Integer.valueOf(consoleScanner.nextLine());
+            printPosts(posts,replyId);
+            replyId = readId(consoleScanner);
         }
     }
 
-    public static void loadPosts(ArrayList<Post> posts) throws FileNotFoundException {
-        File f = new File("posts.txt");
+    public static ArrayList<Post> loadPosts(File f) throws FileNotFoundException {
+        ArrayList<Post> posts = new ArrayList<>();
         Scanner fileScanner = new Scanner(f);
         while (fileScanner.hasNext()) {
             String line = fileScanner.nextLine();
@@ -41,6 +33,22 @@ public class Main {
             Post p = new Post(Integer.valueOf(replyId), author, text);
             posts.add(p);
         }
+        return posts;
+    }
+
+    public static void printPosts(ArrayList<Post> posts,int replyId) {
+        System.out.println("Posts replying to " + replyId);
+        for (int i = 0; i < posts.size(); i++) {
+            Post post = posts.get(i);
+            if(post.replyId == replyId) {
+                System.out.printf("[%s] %s by %s\n", i, post.text, post.author);
+            }
+        }
+    }
+
+    public static int readId(Scanner consoleScanner) {
+        System.out.println("The the id you want to see replies to:");
+        return Integer.valueOf(consoleScanner.nextLine());
     }
 }
 
