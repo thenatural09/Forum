9323c26e749de77dce1b35306716cad88241990b
Troy <tward4@tulane.edu>
2016-09-26 11:00:02 -0400

    Forum that kind of works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,17 +10,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<Post> posts = new ArrayList<>();
 
-        File f = new File("posts.txt");
-        Scanner fileScanner = new Scanner(f);
-        while (fileScanner.hasNext()) {
-            String line = fileScanner.nextLine();
-            String[] columns = line.split("\\|");
-            String replyId = columns[0];
-            String author = columns[1];
-            String text = columns[2];
-            Post p = new Post(Integer.valueOf(replyId), author, text);
-            posts.add(p);
-        }
+        loadPosts(posts);
 
         Scanner consoleScanner = new Scanner(System.in);
 
@@ -36,6 +26,20 @@ public class Main {
 
             System.out.println("The the id you want to see replies to:");
             replyId = Integer.valueOf(consoleScanner.nextLine());
+        }
+    }
+
+    public static void loadPosts(ArrayList<Post> posts) throws FileNotFoundException {
+        File f = new File("posts.txt");
+        Scanner fileScanner = new Scanner(f);
+        while (fileScanner.hasNext()) {
+            String line = fileScanner.nextLine();
+            String[] columns = line.split("\\|");
+            String replyId = columns[0];
+            String author = columns[1];
+            String text = columns[2];
+            Post p = new Post(Integer.valueOf(replyId), author, text);
+            posts.add(p);
         }
     }
 }
